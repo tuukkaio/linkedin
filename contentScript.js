323ab79f5e9c0ed4c556ea1fdc03a3f212b4bb11
tuukkaio <128314510+tuukkaio@users.noreply.github.com>
2023-03-19 15:16:41 -0500

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,29 @@
-function fetchJobDetails() {
+function fetchJobTitle() {
   const jobTitleElement = document.querySelector('.topcard__title');
+  if (!jobTitleElement) {
+    return null;
+  }
+  return jobTitleElement.innerText.trim();
+}
+
+function fetchJobDetails() {
   const companyNameElement = document.querySelector('.topcard__org-name-link');
   const locationElement = document.querySelector('.topcard__flavor--bullet');
   const descriptionElement = document.querySelector('.description__text');
-  
-  if (!jobTitleElement || !companyNameElement || !locationElement || !descriptionElement) {
+
+  if (!companyNameElement || !locationElement || !descriptionElement) {
     return null;
   }
 
-  const jobTitle = jobTitleElement.innerText.trim();
   const companyName = companyNameElement.innerText.trim();
   const location = locationElement.innerText.trim();
   const description = descriptionElement.innerText.trim();
 
-  return { jobTitle, companyName, location, description };
+  return { companyName, location, description };
 }
 
-chrome.runtime.sendMessage({ result: fetchJobDetails() });
+if (window.location.href.includes('linkedin.com/jobs/view')) {
+  const jobTitle = fetchJobTitle();
+  const jobDetails = fetchJobDetails();
+  chrome.runtime.sendMessage({ jobTitle, jobDetails });
+}
