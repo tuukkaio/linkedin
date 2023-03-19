2615e58bf4b703964c3df1528feba979c029480c
tuukkaio <128314510+tuukkaio@users.noreply.github.com>
2023-03-19 15:13:35 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === 'fetchJobDetails') {
-    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-      chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['contentScript.js'] }, ([response]) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError);
-        } else {
-          sendResponse(response.result);
-        }
-      });
+if (request.message === 'fetchJobTitle') {
+  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['contentScript.js'] }, ([response]) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      } else {
+        sendResponse(response.result.jobTitle);
+      }
     });
-    return true;
-  }
-});
+  });
+  return true;
+}
