d64798b93dd56d2b18c9bc576e0c9a0c543150a4
tuukkaio <128314510+tuukkaio@users.noreply.github.com>
2023-03-19 13:11:36 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -61,3 +61,22 @@ async function generateCoverLetter(jobDetails) {
   const data = await response.json();
   return data.choices && data.choices[0] && data.choices[0].text ? data.choices[0].text.trim() : 'Failed to generate cover letter.';
 }
+
+function fetchJobDetails() {
+  const jobTitleElement = document.querySelector('.topcard__title');
+  const companyNameElement = document.querySelector('.topcard__org-name-link');
+  const locationElement = document.querySelector('.topcard__flavor--bullet');
+  const descriptionElement = document.querySelector('.description__text');
+  
+  if (!jobTitleElement || !companyNameElement || !locationElement || !descriptionElement) {
+    return null;
+  }
+
+  const jobTitle = jobTitleElement.innerText.trim();
+  const companyName = companyNameElement.innerText.trim();
+  const location = locationElement.innerText.trim();
+  const description = descriptionElement.innerText.trim();
+
+  return { jobTitle, companyName, location, description };
+}
+
